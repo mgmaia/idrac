293e4c45ff8d0d004e15e727c4334e7e6469b724
Mauricio Maia <67377708+mgmaia@users.noreply.github.com>
2022-01-10 11:04:26 -0300

    Update script_idrac.sh

--- script_idrac.sh
+++ script_idrac.sh
@@ -6,11 +6,11 @@ ${PASSWORD}=pass_test
 for i in {128..139}
 do
 	echo "IP 192.168.15.$i"
-        sshpass -p calvin ssh -o StrictHostKeyChecking=no root@192.168.15.$i "set idrac.users.10.username ${USER}"
-        sshpass -p calvin ssh root@192.168.15.$i "set idrac.users.10.password ${PASSWORD}"
-        sshpass -p calvin ssh root@1192.168.15.$i "set idrac.users.10.enable 1"
-        sshpass -p calvin ssh root@192.168.15.$i "set idrac.users.10.Privilege 0x1ff"
-        sshpass -p calvin ssh root@192.168.15.$i "set idrac.users.10.IpmiLanPrivilege 4"
-        sshpass -p calvin ssh root@192.168.15.$i "set idrac.users.10.IpmiSerialPrivilege 4"
-        sshpass -p calvin ssh root@192.168.15.$i "set idrac.users.10.SolEnable Enabled"
+        sshpass -p root_pwd ssh -o StrictHostKeyChecking=no root@192.168.15.$i "set idrac.users.10.username ${USER}"
+        sshpass -p root_pwd ssh root@192.168.15.$i "set idrac.users.10.password ${PASSWORD}"
+        sshpass -p root_pwd ssh root@1192.168.15.$i "set idrac.users.10.enable 1"
+        sshpass -p root_pwd ssh root@192.168.15.$i "set idrac.users.10.Privilege 0x1ff"
+        sshpass -p root_pwd ssh root@192.168.15.$i "set idrac.users.10.IpmiLanPrivilege 4"
+        sshpass -p root_pwd ssh root@192.168.15.$i "set idrac.users.10.IpmiSerialPrivilege 4"
+        sshpass -p root_pwd ssh root@192.168.15.$i "set idrac.users.10.SolEnable Enabled"
 done
